99e91ff31f990538db67a66ae516d18c6326214f
all4sops2013-ui <all4sops2013@gmail.com>
2025-08-29 16:40:19 +0100

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,24 @@
+name = "john"
+age_john= "january; 26; 2012"
+print(name)
+print(age_john)
+
+name = "sarah"
+age_sarah= "march; 2; 2012"
+print(name)
+print(age_sarah)
+
+name = "ami"
+age_ami= "december; 14; 2011"
+print(name)
+print(age_ami)
+
+name = "daniel"
+age_daniel= "july; 17; 2011"
+print(name)
+print(age_daniel)
+
+name = "felix"
+age_felix = "august; 21; 2012 "
+print(name)
+print(age_felix)
